897f51a037e9b762483b9ada181cda853096ee59
martarguezgarc <mrodriguezgarcia@deloitte.es>
2026-05-05 15:23:56 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+app.config['SECRET_KEY'] = 'mi-clave-super-secreta-hardcodeada-12345'
+
 # ❌ PROBLEMA 3: API key de servicio externo en el código
 EXTERNAL_API_KEY = 'sk-prod-1234567890abcdef9876543210fedcba'
 
